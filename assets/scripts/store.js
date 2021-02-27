81734eae8adf8d1688e47cc386b5453c68dc76cf
Frank Dominguez <elvisf.dominguez@gmail.com>
2021-02-27 16:33:02 -0500

    Add 'component library - flexbox' to store and card styles was updated

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -3,9 +3,15 @@ const DATA = {
   works: [
     {
       name: "Component Library - Essentials",
-      repository: "https://github.com/kenqefh/-component-library-essentials",
+      repository: "https://github.com/kenqefh/component-library-essentials",
       url: "https://kenqefh-component-library-essentials.netlify.app/",
       image: "assets/images/component-library-essentials.png",
+    },
+    {
+      name: "Component Library - Flexbox",
+      repository: "https://github.com/kenqefh/component-library-flexbox",
+      url: "https://kenqefh-component-library-flexbox.netlify.app/",
+      image: "assets/images/component-library-flexbox.png",
     },
   ],
 };
